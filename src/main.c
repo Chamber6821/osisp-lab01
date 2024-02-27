4512ef87ef46a277570ddee0f5ef45ff0f908084
Chamber6821 <vania.osinin@yandex.ru>
2024-02-27 21:02:41 +0300

    feat: parse arguments

--- src/main.c
+++ src/main.c
@@ -1,5 +1,50 @@
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-  printf("Hello World!\n");
+enum Error { UNKNOWN_FLAG = 1, TOO_MANY_PATHS = 2 };
+
+enum Flag { LINK, DIRECTORY, REGULAR_FILE, SORT, FLAG_COUNT };
+
+bool enabledFlags[FLAG_COUNT] = {false};
+
+enum Flag char2flag(char ch) {
+  switch (ch) {
+  case 'l': return LINK;
+  case 'd': return DIRECTORY;
+  case 'f': return REGULAR_FILE;
+  case 's': return SORT;
+  default: printf("Unknown flag: %c\n", ch); exit(UNKNOWN_FLAG);
+  }
+}
+
+bool hasFlags() {
+  for (int i = 0; i < FLAG_COUNT; i++)
+    if (enabledFlags[i]) return true;
+  return false;
+}
+
+bool scanFlags(const char *argument) {
+  if (argument[0] != '-') return false;
+  for (const char *it = argument + 1; it; ++it)
+    enabledFlags[char2flag(*it)] = true;
+  return true;
+}
+
+int main(int argc, char **argv) {
+  char *path = NULL;
+  for (int i = 0; i < argc; i++) {
+    char *argument = argv[i];
+    bool isFlag = scanFlags(argument);
+    if (!isFlag && !path) path = argument;
+    if (!isFlag && path) {
+      printf(
+          "Too many paths: path must be once, but got second path: %s",
+          argument
+      );
+      exit(TOO_MANY_PATHS);
+    }
+  }
+
+  // TODO: Пора бы и вывести в консоль что-нибудь
 }
