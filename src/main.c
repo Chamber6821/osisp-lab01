c8ec890a8d53ad56a8b25df562fc558c06fe74e5
Chamber6821 <vania.osinin@yandex.ru>
2024-02-27 22:47:49 +0300

    refactor: separate flags to filters and modifiers

--- src/main.c
+++ src/main.c
@@ -1,49 +1,80 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 enum Error { UNKNOWN_FLAG = 1, TOO_MANY_PATHS = 2 };
 
-enum Flag { LINK, DIRECTORY, REGULAR_FILE, SORT, FLAG_COUNT };
+enum FilterFlag { UNKNOWN_FILTER, LINK, DIRECTORY, REGULAR_FILE, FILTER_COUNT };
 
-bool enabledFlags[FLAG_COUNT] = {false};
+enum ModifierFlag { UNKNOWN_MODIFIER, SORT, MODIFIER_COUNT };
 
-enum Flag char2flag(char ch) {
+bool filters[FILTER_COUNT] = {false};
+bool modifiers[MODIFIER_COUNT] = {false};
+
+enum FilterFlag char2filter(char ch) {
   switch (ch) {
   case 'l': return LINK;
   case 'd': return DIRECTORY;
   case 'f': return REGULAR_FILE;
-  case 's': return SORT;
-  default: printf("Unknown flag: %c\n", ch); exit(UNKNOWN_FLAG);
+  default: return UNKNOWN_FILTER;
   }
 }
 
-bool hasFlags() {
-  for (int i = 0; i < FLAG_COUNT; i++)
-    if (enabledFlags[i]) return true;
+enum ModifierFlag char2modifier(char ch) {
+  switch (ch) {
+  case 's': return SORT;
+  default: return UNKNOWN_MODIFIER;
+  }
+}
+
+bool hasFilters() {
+  for (int i = 0; i < FILTER_COUNT; i++)
+    if (filters[i]) return true;
   return false;
+}
+
+void enableAllFilters() {
+  for (int i = 0; i < FILTER_COUNT; i++)
+    filters[i] = true;
 }
 
 bool scanFlags(const char *argument) {
   if (argument[0] != '-') return false;
-  for (const char *it = argument + 1; it; ++it)
-    enabledFlags[char2flag(*it)] = true;
+  for (const char *it = argument + 1; *it; ++it) {
+    enum FilterFlag filter = char2filter(*it);
+    if (filter != UNKNOWN_FILTER) {
+      filters[filter] = true;
+      continue;
+    }
+
+    enum ModifierFlag modifier = char2modifier(*it);
+    if (modifier != UNKNOWN_MODIFIER) {
+      modifiers[modifier] = true;
+      continue;
+    }
+
+    printf(
+        "Unknown flag '%c' (%d) in argument '%s'\n",
+        *it,
+        (int)*it,
+        argument
+    );
+    exit(UNKNOWN_FLAG);
+  }
   return true;
 }
 
 int main(int argc, char **argv) {
-  char *path = NULL;
-  for (int i = 0; i < argc; i++) {
+  const char *path = NULL;
+  for (int i = 1; i < argc; i++) {
     char *argument = argv[i];
     bool isFlag = scanFlags(argument);
-    if (!isFlag && !path) path = argument;
     if (!isFlag && path) {
-      printf(
-          "Too many paths: path must be once, but got second path: %s",
-          argument
-      );
+      printf("Path must be once, but got second path: %s\n", argument);
       exit(TOO_MANY_PATHS);
     }
+    if (!isFlag && !path) path = argument;
   }
 
   // TODO: Пора бы и вывести в консоль что-нибудь
